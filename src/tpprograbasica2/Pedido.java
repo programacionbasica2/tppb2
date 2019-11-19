d68cf25618721d05d6fd6befdeb8cc48e55068b5
JuanRugna <55444429+JuanRugna@users.noreply.github.com>
2019-11-19 20:08:46 -0300

    Update Pedido.java

--- src/tpprograbasica2/Pedido.java
+++ src/tpprograbasica2/Pedido.java
@@ -1,1 +1,27 @@
+package tpprograbasica2;
 
+public class Pedido {
+	private Integer nOrden;
+	private Integer nMesa;
+	
+	public Pedido (Integer nOrden, Integer nMesa) {
+		this.nOrden=nOrden;
+		this.nMesa=nMesa;
+	}
+
+	public Integer getnOrden() {
+		return nOrden;
+	}
+
+	public void setnOrden(Integer nOrden) {
+		this.nOrden = nOrden;
+	}
+
+	public Integer getnMesa() {
+		return nMesa;
+	}
+
+	public void setnMesa(Integer nMesa) {
+		this.nMesa = nMesa;
+	}
+}
