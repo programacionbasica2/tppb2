3f1fbd70c7954be61e8b2c88cce91de8e269e774
juanc <juanc@DESKTOP-0V9KVCE>
2019-11-19 21:12:11 -0300

    Se actualizo la clase Restaurante

--- src/tpprograbasica2/Restaurante.java
+++ src/tpprograbasica2/Restaurante.java
@@ -1,1 +1,17 @@
+package tpprograbasica2;
 
+import java.util.ArrayList;
+
+public class Restaurante {
+	ArrayList <Producto> listaProductos = new ArrayList <Producto>();
+	ArrayList <Usuario> listaUsuarios = new ArrayList <Usuario>();
+	
+	public void realizarPedido(Integer id) {
+		for(Producto daux: listaProductos) {
+			if(daux.getId()==id) {
+				
+			}
+		}
+	}
+	
+}
