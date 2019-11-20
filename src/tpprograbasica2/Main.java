9b5a5342b7d333033a1fb18f41d060ba877c902e
juan sebastian rugna <juanrugna@hotmail.com>
2019-11-20 14:45:23 -0300

    Arranque el login

---
+++ src/tpprograbasica2/Main.java
@@ -0,0 +1,55 @@
+package tpprograbasica2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Restaurante UnlamResto = new Restaurante();
+		Usuario test1 = new Usuario("Juan", "Rugna", "juanrugna@hotmail.com", "asd");
+		Admin test2 = new Admin("Roman", "Riquelme", "romanriquelme@hotmail.com", "asd");
+		Scanner teclado = new Scanner(System.in);
+		UnlamResto.listaUsuarios.add(test1);
+
+		System.out.println("Seleccione 1 para ingresar en el sistema");
+		System.out.println("Seleccione 2 para registrarse en el sistema");
+		Integer opcion = teclado.nextInt();
+		switch (opcion) {
+		case 1: // Login
+			System.out.println("Ingrese el mail");
+			String mail = teclado.next();
+			System.out.println("Ingrese la contraseña");
+			String contrasena = teclado.next();
+			try {
+				UnlamResto.ingresarAlSistema(mail, contrasena);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			break;
+
+		case 2:  //Registro
+			System.out.println("Ingrese su nombre");
+			String nombre = teclado.next();
+			System.out.println("Ingrese su apellido");
+			String apellido = teclado.next();
+			System.out.println("Ingrese su mail");
+			String mail2 = teclado.next();
+			System.out.println("Ingrese su contraseña");
+			String contrasena2 = teclado.next();
+			try {
+				UnlamResto.registroUsuario(nombre, apellido, mail2, contrasena2);
+				System.out.println("Usuario registrado correctamente");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		default:
+			break;
+		}
+	}
+
+}
--- src/tpprograbasica2/Restaurante.java
+++ src/tpprograbasica2/Restaurante.java
@@ -1,11 +1,47 @@
 package tpprograbasica2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Restaurante {
 	ArrayList <Producto> listaProductos = new ArrayList <Producto>(); //Listas
-	ArrayList <Usuario> listaUsuarios = new ArrayList <Usuario>();
+	HashSet <Usuario> listaUsuarios = new HashSet <Usuario>();
+	HashSet <Admin> listaAdmins = new HashSet <Admin>();
 	
 	
+	public Boolean registroUsuario(String nombre, String apellido, String mail, String contrasena) throws Exception {
+		Usuario usuario =new Usuario("","","","");
+		usuario.setNombre(nombre);
+		usuario.setApellido(apellido);
+		for(Usuario daux: listaUsuarios) {
+			if(daux.getMail().equals(mail)) {
+				throw new Exception ("El mail ya se encuentra en uso");
+			}else {
+				usuario.setMail(mail);
+			}
+		}
+		usuario.setContrasena(contrasena);
+		return true;
+	}
+	
+	
+	public Integer ingresarAlSistema(String mail, String contrasena) throws Exception {
+		
+		for(Usuario daux: listaUsuarios) {
+			if(daux.getMail()==mail && daux.getContrasena()==contrasena) {
+				return 1;
+			}else {
+				for(Admin daux2: listaAdmins) {
+					if(daux2.getMail()==mail && daux2.getContrasena()==contrasena) {
+						return 2;
+					}else {
+						throw new Exception ("Datos invalidos");
+					}
+					
+				}
+			}
+		}
+	return -1;
+	}
 	
 }
